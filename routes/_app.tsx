5c13d7457499ca075daaaffcc72e33de271ff48b
Teelirium <angryzombi01@gmail.com>
2023-10-02 15:47:58 +0500

    add og title

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,13 +1,14 @@
-import { AppProps } from '$fresh/server.ts';
+import { AppProps } from "$fresh/server.ts";
 
 export default function App({ Component }: AppProps) {
   return (
     <html>
       <head>
-        <meta charSet='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <link rel='stylesheet' href='tailwind.css' />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="tailwind.css" />
         <title>Teel@Countries</title>
+        <meta content="Teel@Countries" property="og:title" />
       </head>
       <body>
         <Component />
