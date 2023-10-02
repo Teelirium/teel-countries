45a09853b48efdbf9ac212461bbd8d56964b1874
Teelirium <angryzombi01@gmail.com>
2023-10-02 17:22:29 +0500

    import data from the sheet

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,10 @@
-import { PageProps } from '$fresh/server.ts';
-import { CountryListItem } from '@/components/CountryListItem.tsx';
-import { Layout } from '@/components/Layout.tsx';
-import countries from '@/data/countries.js';
-import visited from '@/data/visited.js';
-import { Country } from '@/types/Country.ts';
-import { classes } from '@/util/classes.ts';
+import { PageProps } from "$fresh/server.ts";
+import { CountryListItem } from "@/components/CountryListItem.tsx";
+import { Layout } from "@/components/Layout.tsx";
+import countries from "@/data/countries.js";
+import visited from "@/data/visited.js";
+import { Country } from "@/types/Country.ts";
+import { classes } from "@/util/classes.ts";
 
 function isVisited(country: Country): boolean {
   return visited.some((v) => v[1] === country.cca3);
@@ -13,11 +13,15 @@ function isVisited(country: Country): boolean {
 export default function Countries(props: PageProps) {
   return (
     <Layout path={props.route}>
-      <section class='max-w-screen-md mx-auto flex flex-col items-center justify-center gap-4'>
-        <h1 class='text-3xl font-bold'>Список всех* стран**</h1>
-        <ol class='flex flex-col gap-2'>
+      <section class="max-w-screen-md mx-auto flex flex-col items-center justify-center gap-4">
+        <h1 class="text-3xl font-bold">Список всех* стран**</h1>
+        <ol class="flex flex-col gap-2">
           {countries.map((c, i) => (
-            <CountryListItem country={c} index={i} isVisited={isVisited(c)} />
+            <CountryListItem
+              country={c}
+              index={i + 1}
+              isVisited={isVisited(c)}
+            />
           ))}
         </ol>
       </section>
